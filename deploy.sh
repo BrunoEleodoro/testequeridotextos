dffd391770426fbc868e39cf9f88bf5cea232f40
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-05-28 22:17:14 -0300

    aa

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
-rm -rf testequeridotextos
+# rm -rf testequeridotextos
 # git clone https://bot-queridotextos:atualizartextos1@github.com/BrunoEleodoro/testequeridotextos
 # cd testequeridotextos
 date '+%Y-%m-%d %H:%M:%S' > last_update.txt
 curl "https://www.googleapis.com/blogger/v3/blogs/2771518682926329856/posts?fetchImages=true&maxResults=200&key=AIzaSyBt2eRbKEj-wMbDDuRo9QAVR6FP2EeYXnk" > data.json
 git init
-git add *
+git add .
 git commit -m "${DATE}"
 git config user.email "travis@travis.org"
 git config user.name "travis" # this email and name can be set anything you like
